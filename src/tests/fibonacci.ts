e595955cc662f2eb4d72555bef7a8f50b52c926b
Framerage <officialigonin@mail.ru>
2026-02-12 21:10:15 +0400

    edit fibo

--- src/tests/fibonacci.ts
+++ src/tests/fibonacci.ts
@@ -5,23 +5,16 @@ const fibo = (num: number) => {
   return num < 0 ? NaN : num === 0 ? 0 : 1;
 };
 
-const fiboNew = (n: number) => {
-  if (n < 0) return NaN;
-  if (n === 0) return 0;
-  if (n === 1) return 1;
+const fiboNew = (num: number) => {
+  if (num > 1) {
+    let prev = 0;
+    let curr = 1;
 
-  let prev = 0;
-  let curr = 1;
+    for (let i = 2; i <= num; i++) {
+      [prev, curr] = [curr, prev + curr];
+    }
 
-  for (let i = 2; i <= n; i++) {
-    [prev, curr] = [curr, prev + curr];
-    // const next = prev + curr;
-    //   console.log(next, ' next - prev ',prev,'cur',curr)
-    // prev = curr;
-    // curr = next;
-    //   console.log(prev,'prev new')
-    //   console.log(curr,'curr new')
+    return curr;
   }
-
-  return curr;
+  return num < 0 ? NaN : num === 0 ? 0 : 1;
 };
